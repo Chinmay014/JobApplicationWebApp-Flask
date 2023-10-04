69872a03f8893f679d4f4eaabec05ed1d56b6de9
Chinmay014 <chinmays117@gmail.com>
2023-10-04 07:44:44 +0200

    send confirmation email

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "app123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "chinmays117@gmail.com"
+app.config["MAIL_PASSWORD"] = os.getenv("PASSWORD")
+
 db = SQLAlchemy(app)
 
+mail = Mail(app)
 
 class Form(db.Model):
     id = db.Column(db.Integer,primary_key=True)
@@ -32,6 +41,13 @@ def index():
         form = Form(name=first_name,surname=last_name,email=email,date=date_obj,occupation=occupation)
         db.session.add(form)
         db.session.commit()
+        message_body = f"Thank you for your submission, {first_name}! Here are the details we received:\n"\
+        f"your name: {first_name},\n last name: {last_name} \n date:{date}\n We will get in touch soon!"
+        print(message_body)
+        print(app.config["MAIL_PASSWORD"])
+        message=Message(subject="New Form Submission",sender=app.config["MAIL_USERNAME"],
+                recipients=[email],body=message_body)
+        mail.send(message)
         flash(f"{first_name}, your form was sent successfully!","success")
         print("You Here!")
 
